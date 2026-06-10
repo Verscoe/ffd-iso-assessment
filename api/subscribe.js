d1f216cfb5df719d7fc708fba62b3a621058e94c
Verscoe <freedomfromdissonance@gmail.com>
2026-06-10 00:39:29 -0500

    Add iso-assessment Mailchimp tag to subscriber payload

--- api/subscribe.js
+++ api/subscribe.js
@@ -10,7 +10,6 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
-
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -35,6 +34,7 @@ export default async function handler(req, res) {
       LNAME: last,
     },
     tags: [
+      'iso-assessment',
       dept ? `dept:${dept}` : null,
       isoClass ? `iso-class:${isoClass}` : null,
     ].filter(Boolean),
